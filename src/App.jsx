875addff99993a4ba5e4722a99a8d227308bc2ad
sourav55555 <souravsinghassr555@gmail.com>
2023-03-31 14:28:11 +0600

    question ans

--- src/App.jsx
+++ src/App.jsx
@@ -49,9 +49,33 @@ function App() {
         </div>
         <SideBar timeTotal={timeTotal} bookmarks={bookmarks}></SideBar>
       </div>
+      <div className='my-10 mx-8'>
+        <div className='mb-4 border-2 rounded-lg p-4'>
+          <p className='my-3 border-b-2 pb-3 font-semibold'>What is the difference between props and state?</p>
+          <p>
+            We can change state values, but props values can not be changed. <br />
+             Wc can set different components values or data on stat, with props we can pass data from one component to another. 
+          </p>
+        </div>
+        <div className='mb-4 border-2 rounded-lg p-4'>
+          <p className='my-3 border-b-2 pb-3 font-semibold'>How does useState work?</p>
+          <p>
+            useStat is a react hook. It has two functional components. First, we need to pass any default value and   it returns a variable with this current value. And using another component we can set or update the   current variable value. Then we can get the latest updated value.
+          </p>
+        </div>
+        <div className='mb-4 border-2 rounded-lg p-4'>
+          <p className='my-3 border-b-2 pb-3 font-semibold'>Purpose of useEffect other than fetching data.</p>
+          <p>useEffect is a react hook. We can set dependencies on useEffect. Using dependencies it will run at the beginning, it will run if the dependencies value is changed. So that we can get live update on any dynamic data.</p>
+        </div>
+        <div className='mb-4 border-2 rounded-lg p-4'>
+          <p className='my-3 border-b-2 pb-3 font-semibold'>How Does React work?</p>
+          <p>
+          React is a UI-based JavaScript library. React works with virtual DOM which is a DOM tree representation in javascript. The virtual DOM and the browser DOM are the same. When we read and change the DOM in react it uses a virtual representation of it. And, after changing the virtual DOM it will update the browser DOM according to the virtual DOM.
+          </p>
+        </div>
+      </div>
       <ToastContainer/>
     </div>
   )
-}
-
-export default App
+        }
+export default App;
